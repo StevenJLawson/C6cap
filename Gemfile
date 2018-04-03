5cb7b4e56559b8cdae5ea10089d817d51792c1a9
Steven Lawson <sjlawson@gmail.com>
2018-04-03 16:06:57 -0700

    More stuff

--- Gemfile
+++ Gemfile
@@ -7,6 +7,9 @@ gem 'rails-api', '~>0.4', '>=0.4.0'
 gem 'jbuilder', '~> 2.0', '>=2.6.0'
 group :development do
 	gem 'spring', '~>2.0', '>=2.0.0'
+	gem 'byebug'
 	gem 'tzinfo-data', :platforms=>[:mingw, :mswin, :x64_mingw, :jruby]
+	gem 'rspec-rails', '~>3.5', '>=3.5.2'
 end
 gem 'pg', '~>0.19', '>=0.19.0'
+gem 'mongoid', '~>5.1', '>=5.1.5'
--- config/application.rb
+++ config/application.rb
@@ -17,6 +17,14 @@ Bundler.require(*Rails.groups)
 
 module Capstone
   class Application < Rails::Application
+
+  	Mongoid.load!('./config/mongoid.yml')
+  	config.generators {|g| g.orm :active_record}
+  	#config.generators {|g| g.orm :mongoid}
+
+
+	config.active_record.raise_in_transactional_callbacks = true 
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
